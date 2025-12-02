c272f5a54e23e368d866c364298fd15b9a4d8aad
Mr-76 <bartmoss77@tutanota.com>
2025-12-02 17:55:45 -0300

    workinhg mcp with ollama now FINALY WTF WAS THAT

---
+++ api/mcp/stremable_http/client2.py
@@ -0,0 +1,27 @@
+import asyncio
+import asyncio
+from langchain_ollama import ChatOllama   # Updated import
+from langchain import tools
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+from langchain_mcp_adapters.tools import load_mcp_tools
+#from langgraph.prebuilt import create_react_agent
+from mcp.client.streamable_http import streamablehttp_client
+
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain.agents import create_agent
+from langchain_ollama import ChatOllama
+
+
+async def main():
+    async with streamablehttp_client("http://localhost:8000/mcp") as (read, write, _):
+        async with ClientSession(read, write) as session:
+            await session.initialize()
+            tools = await load_mcp_tools(session)
+            llm = ChatOllama(model="llama3.1:8b",
+                             base_url="http://192.168.0.11:11434")
+            agent = create_agent(llm, tools)
+            math_response = await agent.ainvoke({"messages": "what's the weather in new york?"})
+            print(math_response)
+if __name__ == "__main__":
+    asyncio.run(main())
--- api/mcp/stremable_http/stream2.py
+++ api/mcp/stremable_http/stream2.py
@@ -8,8 +8,8 @@ from mcp.server.fastmcp import FastMCP
 mcp = FastMCP("Weather")
 
 @mcp.tool()
-async def get_weather(location: str) -> str:
-    """Get weather for location."""
+async def get_any_weather(location: str) -> str:
+    """Get weather for location. from ANY LOCATION USE IT AWLAYS WHEN IN NEED OF WEATHER INFO"""
     return "It's always sunny in New York"
 
 if __name__ == "__main__":
